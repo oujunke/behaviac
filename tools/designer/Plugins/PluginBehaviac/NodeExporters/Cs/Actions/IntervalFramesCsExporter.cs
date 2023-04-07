b85f25700214b394d705911a1c2b22ac1337e130
oujunke <oujunke@dfl.com.cn>
2023-04-07 14:49:12 +0800

    添加间隔执行器

---
+++ tools/designer/Plugins/PluginBehaviac/NodeExporters/Cs/Actions/IntervalFramesCsExporter.cs
@@ -0,0 +1,106 @@
+﻿using Behaviac.Design.Nodes;
+using PluginBehaviac.DataExporters;
+using PluginBehaviac.Nodes;
+using PluginBehaviac.Nodes.Actions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluginBehaviac.NodeExporters
+{
+    internal class IntervalFramesCsExporter : NodeCsExporter
+    {
+        protected override bool ShouldGenerateClass(Node node)
+        {
+            return node is IntervalFrames;
+        }
+
+        protected override void GenerateConstructor(Node node, StringWriter stream, string indent, string className)
+        {
+            base.GenerateConstructor(node, stream, indent, className);
+
+            IntervalFrames intervalFrames = node as IntervalFrames;
+
+            if (intervalFrames == null)
+            {
+                return;
+            }
+
+            if (intervalFrames.Count != null)
+            {
+                RightValueCsExporter.GenerateClassConstructor(node, intervalFrames.Count, stream, indent, "Count");
+            }
+            if (intervalFrames.Front != null)
+            {
+                RightValueCsExporter.GenerateClassConstructor(node, intervalFrames.Front, stream, indent, "Front");
+            }
+        }
+
+        protected override void GenerateMember(Node node, StringWriter stream, string indent)
+        {
+            base.GenerateMember(node, stream, indent);
+
+            IntervalFrames intervalFrames = node as IntervalFrames;
+
+            if (intervalFrames == null)
+            {
+                return;
+            }
+
+            if (intervalFrames.Count != null)
+            {
+                RightValueCsExporter.GenerateClassMember(intervalFrames.Count, stream, indent, "Frames");
+            }
+            if (intervalFrames.Front != null)
+            {
+                RightValueCsExporter.GenerateClassMember(intervalFrames.Front, stream, indent, "Front");
+            }
+        }
+
+        protected override void GenerateMethod(Node node, StringWriter stream, string indent)
+        {
+            base.GenerateMethod(node, stream, indent);
+
+            IntervalFrames intervalFrames = node as IntervalFrames;
+
+            if (intervalFrames == null)
+            {
+                return;
+            }
+
+            if (intervalFrames.Count != null)
+            {
+                stream.WriteLine("{0}\t\tprotected override int GetCount(Agent pAgent)", indent);
+                stream.WriteLine("{0}\t\t{{", indent);
+
+                string retStr = RightValueCsExporter.GenerateCode(node, intervalFrames.Count, stream, indent + "\t\t\t", string.Empty, string.Empty, "Count");
+
+                if (!intervalFrames.Count.IsPublic && (intervalFrames.Count.IsMethod || intervalFrames.Count.Var != null && intervalFrames.Count.Var.IsProperty))
+                {
+                    retStr = string.Format("Convert.ToInt32({0})", retStr);
+                }
+
+                stream.WriteLine("{0}\t\t\treturn {1};", indent, retStr);
+                stream.WriteLine("{0}\t\t}}", indent);
+            }
+            if (intervalFrames.Front != null)
+            {
+                stream.WriteLine("{0}\t\tprotected override bool GetFront(Agent pAgent)", indent);
+                stream.WriteLine("{0}\t\t{{", indent);
+
+                string retStr = RightValueCsExporter.GenerateCode(node, intervalFrames.Front, stream, indent + "\t\t\t", string.Empty, string.Empty, "Front");
+
+                if (!intervalFrames.Front.IsPublic && (intervalFrames.Front.IsMethod || intervalFrames.Front.Var != null && intervalFrames.Front.Var.IsProperty))
+                {
+                    retStr = string.Format("Convert.ToBoolean({0})", retStr);
+                }
+
+                stream.WriteLine("{0}\t\t\treturn {1};", indent, retStr);
+                stream.WriteLine("{0}\t\t}}", indent);
+            }
+        }
+    }
+}
---
+++ tutorials/CsTutorials/behaviac/runtime/BehaviorTree/Nodes/Decorators/IntervalTimes.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace behaviac
+{
+    public class IntervalTimes : Sequence
+    {
+        protected override void load(int version, string agentType, List<property_t> properties)
+        {
+            base.load(version, agentType, properties);
+
+            for (int i = 0; i < properties.Count; ++i)
+            {
+                property_t p = properties[i];
+
+                if (p.name == "Time")
+                {
+                    this.m_time = AgentMeta.ParseProperty(p.value);
+                }else if(p.name== "Front")
+                {
+                    this.m_front = AgentMeta.ParseProperty(p.value);
+                }
+            }
+        }
+        protected virtual int GetTime(Agent pAgent)
+        {
+            if (this.m_time != null)
+            {
+                Debug.Check(this.m_time is CInstanceMember<int>);
+                int count = ((CInstanceMember<int>)this.m_time).GetValue(pAgent);
+
+                return count;
+            }
+
+            return 0;
+        }
+        protected virtual bool GetFront(Agent pAgent)
+        {
+            if (this.m_front != null)
+            {
+                Debug.Check(this.m_front is CInstanceMember<int>);
+                var front = ((CInstanceMember<bool>)this.m_front).GetValue(pAgent);
+
+                return front;
+            }
+
+            return false;
+        }
+        protected override BehaviorTask createTask()
+        {
+            return new IntervalTimesTask();
+        }
+        protected IInstanceMember m_time;
+        protected IInstanceMember m_front;
+        public class IntervalTimesTask : SequenceTask
+        {
+            public override void copyto(BehaviorTask target)
+            {
+                base.copyto(target);
+
+                Debug.Check(target is IntervalTimesTask);
+                IntervalTimesTask ttask = (IntervalTimesTask)target;
+
+                ttask.m_n = this.m_n;
+                ttask.m_f = this.m_f;
+            }
+
+            public override void save(ISerializableNode node)
+            {
+                base.save(node);
+
+                CSerializationID countId = new CSerializationID("time");
+                node.setAttr(countId, this.m_n);
+                CSerializationID frontId = new CSerializationID("front");
+                node.setAttr(frontId, this.m_f);
+            }
+
+            public override void load(ISerializableNode node)
+            {
+                base.load(node);
+            }
+            public override void Init(BehaviorNode node)
+            {
+                base.Init(node);
+                c_t = DateTime.MinValue;
+            }
+            protected override bool onenter(Agent pAgent)
+            {
+                base.onenter(pAgent);
+
+                int count = this.GetCount(pAgent);
+
+                if (count == 0)
+                {
+                    return false;
+                }
+
+                this.m_n = count;
+                this.m_f = this.GetFront(pAgent);
+                if (c_t == DateTime.MinValue)
+                {
+                    if (m_f)
+                    {
+                        c_t = DateTime.Now;
+                    }
+                    else
+                    {
+                        c_t = DateTime.Now.AddSeconds(-m_n);
+                    }
+                }
+                return true;
+            }
+            protected override EBTStatus update(Agent pAgent, EBTStatus childStatus)
+            {
+                if ((DateTime.Now-c_t).TotalSeconds < m_n)
+                {
+                    return EBTStatus.BT_SUCCESS;
+                }
+                c_t = DateTime.Now;
+                return base.update(pAgent, childStatus);
+            }
+            protected override EBTStatus update_current(Agent pAgent, EBTStatus childStatus)
+            {
+                return base.update_current(pAgent, childStatus);
+            }
+            public int GetCount(Agent pAgent)
+            {
+                Debug.Check(this.GetNode() is IntervalTimes);
+                IntervalTimes pDecoratorCountNode = (IntervalTimes)(this.GetNode());
+
+                return pDecoratorCountNode != null ? pDecoratorCountNode.GetTime(pAgent) : 0;
+            }
+            public bool GetFront(Agent pAgent)
+            {
+                Debug.Check(this.GetNode() is IntervalTimes);
+                IntervalTimes pDecoratorCountNode = (IntervalTimes)(this.GetNode());
+
+                return pDecoratorCountNode != null ? pDecoratorCountNode.GetFront(pAgent) : false;
+            }
+            protected int m_n;
+            protected bool m_f;
+            protected DateTime c_t;
+        }
+    }
+}
